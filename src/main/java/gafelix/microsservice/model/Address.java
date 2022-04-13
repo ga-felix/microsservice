886ffdb6b6c1098ddaf5a10fe20b657f3a319685
ga-felix <gafelix435@gmail.com>
2022-04-13 19:54:33 -0300

    feat(model): create jpa entities

---
+++ src/main/java/gafelix/microsservice/model/Address.java
@@ -0,0 +1,27 @@
+package gafelix.microsservice.model;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+
+    @Id @GeneratedValue
+    private Long id;
+    @NotNull
+    private String country;
+    @NotNull
+    private String state;
+    @NotNull
+    private String county;
+    @NotNull
+    private String zipCode;
+    @NotNull
+    private String street;
+    private Long streetNumber;
+    private String extra;
+
+}
---
+++ src/main/java/gafelix/microsservice/model/User.java
@@ -0,0 +1,29 @@
+package gafelix.microsservice.model;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String email;
+    @OneToMany
+    @NotNull
+    private List<Address> address;
+    @NotNull
+    private String CPF;
+    private String PIS;
+    @NotNull
+    private String password;
+
+}
